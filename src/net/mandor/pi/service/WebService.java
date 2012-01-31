7f7c932b6a5722ee88f4f106f8177f00b54c9523
Mandor <mandor@mandor.net>
2012-01-31 22:20:36 +0100

    Tweaked the webservice's executor.

--- src/net/mandor/pi/service/WebService.java
+++ src/net/mandor/pi/service/WebService.java
@@ -1,6 +1,8 @@
 package net.mandor.pi.service;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.xml.ws.Endpoint;
 
@@ -8,27 +10,36 @@ import org.apache.log4j.Logger;
 
 import net.mandor.pi.engine.Engine;
 
-/** Contains the webservice being published as well as the search engine. */
+/** Contains the webservice's endpoint and executor. */
 public final class WebService {
 	
-	/** Full address including port of the published webservice. */
-	private static final String ADDRESS = "service.address";
-	/** Service's logger. */
+	/** Configuration key for the endpoint's URL and port. */
+	private static final String URL = "service.url";
+	/** Configuration key for the size of the executor's thread pool. */ 
+	private static final String THREADS = "service.threads";
+	/** Webservice's logger. */
 	private static final Logger L = Logger.getLogger(WebService.class);
-	/** Endpoint of the webservice published by this class. */
+	/** Executor used by the endpoint to dispatch requests. */
+	private ExecutorService executor;
+	/** Endpoint of the webservice. */
 	private Endpoint endpoint;
 	
 	/**
-	 * @param p Configuration of the webservice and search engine.
-	 * @param e Search engine's instance.
+	 * Initializes and publishes the webservice's endpoint.
+	 * @param p Configuration of the webservice.
+	 * @param e Instance of the search engine.
 	 */
 	public WebService(final Properties p, final Engine e) {
-		String s = p.getProperty(ADDRESS);
-		L.debug("Publishing webservice at: " + s);
-		endpoint = Endpoint.publish(s, new SearchService(e));
+		int i = Integer.valueOf(p.getProperty(THREADS));
+		executor = Executors.newFixedThreadPool(i);
+		L.debug("Using executor " + executor + " with " + i + " threads.");
+		endpoint = Endpoint.create(new SearchService(e));
+		endpoint.setExecutor(executor);
+		endpoint.publish(p.getProperty(URL));
+		L.info("Webservice published at: " + p.getProperty(URL));
 	}
 	
-	/** Stops the webservice and closes the search engine. */
-	public void close() { endpoint.stop(); }
+	/** Stops the webservice and shuts down its executor. */
+	public void close() { executor.shutdown(); endpoint.stop(); }
 
 }
